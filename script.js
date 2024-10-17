abdbcf371cef1685836e054f445945862acc3a26
parangat11 <parangataditya001@gmail.com>
2024-10-17 05:30:09 +0530

    Add styles and a delete button (functionality remaining).

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const btn = document.querySelector('.add');
-const diaBox = document.querySelector('#new-book');
+const btn = document.querySelector('#add');
+const diaBox = document.querySelector('#new-spell');
 const spellName = document.querySelector('#spell-name');
 const spellAuthor = document.querySelector('#author')
 const confBtn = document.querySelector('#conf-btn');
 
 let spellReportoire = new Array();
+let index = 0;
 
 btn.addEventListener('click', (e) => {
     diaBox.showModal();
@@ -15,27 +16,34 @@ confBtn.addEventListener('click', (e) => {
     let newSpell = {};
     newSpell.name = spellName.value;
     newSpell.author = spellAuthor.value;
-
+    
     // Makeshift:
     spellReportoire.push(newSpell);
-
+    
     spellName.value = null;
     spellAuthor.value = null;
-
+    
     // Adding the spell to our DOM with class .spell-i:
     const div = document.createElement('div');
-    div.classList.add(`spell spell-${spellReportoire.length}`);
+    div.classList.add(`spell-${index}`);
+    div.classList.add('spell');
+    index++;
     const spellBlock = document.querySelector('.spells');
     console.log(spellBlock);
     console.log(spellBlock.textContent);
     const emptyElement = document.querySelector('#empty');
-    emptyElement.remove();
+    if(emptyElement)
+        emptyElement.remove();
     for(const val in newSpell) {
         const col = document.createElement('div');
         col.textContent = newSpell[val];
         div.appendChild(col);
     }
+    const delButton = document.createElement('button');
+    delButton.textContent = 'x';
+    delButton.classList.add(`del-${index}`);
+    div.appendChild(delButton);
     spellBlock.appendChild(div);
-
+    
     diaBox.close();
 });
